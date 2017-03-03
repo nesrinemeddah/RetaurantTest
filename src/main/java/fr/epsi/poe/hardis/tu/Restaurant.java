bba0593021d90ba94b8521971492d89a83e25a6c
Bennani Ismail <is.bennani@gmail.com>
2017-03-03 14:57:52 +0100

    commit pour travis

---
+++ src/main/java/fr/epsi/poe/hardis/tu/Restaurant.java
@@ -0,0 +1,61 @@
+package fr.epsi.poe.hardis.tu;
+
+/**
+ * Created by medda on 28/02/2017.
+ */
+public class Restaurant {
+    private String nom;
+    private String adresse;
+    private String codePostale;
+    private String numeroDeTelephone;
+    private boolean ouvert = true;
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = "scoopo";
+
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String conflit() {
+     return ("moi test  d'abord ") ;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = "adresse_Con___flit"  ;
+    }
+
+    public String getCodePostale() {
+        return codePostale;
+    }
+
+    public void setCodePostale(String codePostale) {
+        this.codePostale = "69000";
+    }
+
+    public String getNumeroDeTelephone() {
+        return numeroDeTelephone;
+    }
+
+    public void setNumeroDeTelephone(String numeroDeTelephone) {
+       if (numeroDeTelephone != null && !"".equals(numeroDeTelephone) && numeroDeTelephone.startsWith("0033")) {
+                this.numeroDeTelephone = numeroDeTelephone;
+        }
+    }
+
+    public boolean isOuvert() {
+        return ouvert;
+    }
+
+    public void setOuvert(boolean ouvert) {
+        this.ouvert = ouvert;
+    }
+
+}
---
+++ src/test/java/fr/epsi/poe/hardis/tu/RestaurantTest.java
@@ -0,0 +1,62 @@
+package fr.epsi.poe.hardis.tu;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/* import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*; */
+
+/**
+ * Created by medda on 28/02/2017.
+ */
+public class RestaurantTest {
+
+
+    public static final String STRING_N = "00336123456789";
+
+    public RestaurantTest() {
+    }
+
+    @Test
+    public void testThatRestaurantIsOpenByDefault() {
+        // Arrange
+        Restaurant restaurant = new Restaurant();
+        // Act
+        boolean verifie = restaurant.isOuvert();
+        // Assert
+        Assert.assertTrue(verifie);
+    }
+
+ /*   @Test
+    public  void conflit () {
+    }*/
+
+
+    @Test
+    public void testThatValidPhoneNumberIsCorrectlySaved() {
+        Restaurant restaurant = new Restaurant();
+        String number = STRING_N;
+        restaurant.setNumeroDeTelephone(number);
+        Assert.assertEquals("",number, restaurant.getNumeroDeTelephone());
+    }
+
+    @Test
+    public void testThatInValidPhoneNumberIsNotSaved() {
+        Restaurant restaurant = new Restaurant();
+        String number = "006123456789";
+        restaurant.setNumeroDeTelephone(number);
+        Assert.assertNotEquals("",number, restaurant.getNumeroDeTelephone());
+    }
+
+   /* @Test
+    public void testThatRestaurantIsNotOpen() {
+        // Arrange
+        Restaurant restaurant = new Restaurant();
+        // Act
+        boolean verifie = restaurant.isOuvert();
+        // Assert
+        Assert.assertFalse(verifie);
+    }*/
+}
